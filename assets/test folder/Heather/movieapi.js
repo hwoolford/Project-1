f4edf38c91bae96a6e58567abe10f9bfe5558489
forxtoisate <hwoolford@gmail.com>
2023-11-13 19:56:36 -0600

    updated heather's test files

--- assets/test folder/Heather/movieapi.js
+++ assets/test folder/Heather/movieapi.js
@@ -1,56 +1,84 @@
-const imagePath = "https://image.tmdb.org/t/p/w500/";
-const apiKey = "9647c3387122f212c6ed1ddeb758ffb2"
+const imagePath = "https://image.tmdb.org/t/p/w154/";
+const apiKey = "9647c3387122f212c6ed1ddeb758ffb2";
 const form = document.getElementById("user-form");
 let formInput = document.getElementById("search-input");
 const submitBtn = document.getElementById("btn");
 const main = document.getElementById("main");
-const searchInput = formInput.value.trim();
-let movieAuth = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjQ3YzMzODcxMjJmMjEyYzZlZDFkZGViNzU4ZmZiMiIsInN1YiI6IjY1NGQwZGE0ZmQ0ZjgwMDExZWQzZDhjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HB0iJ-sayz4Ungi3ekOAARph1iwM4wQe_pzuZsOeyzQ",
-    },
-  };
 
-let movieURL = `https://api.themoviedb.org/3/search/movie?include_adult=false&original_language=en-US&page=1&query=${searchInput}`
+const movieAuth = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjQ3YzMzODcxMjJmMjEyYzZlZDFkZGViNzU4ZmZiMiIsInN1YiI6IjY1NGQwZGE0ZmQ0ZjgwMDExZWQzZDhjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HB0iJ-sayz4Ungi3ekOAARph1iwM4wQe_pzuZsOeyzQ",
+  },
+};
 
-function findMovies() {
-      fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&original_language=en-US&page=1&query=${searchInput}`, movieAuth)
+function findMovies(search) {
+  let movieURL = `https://api.themoviedb.org/3/search/movie?include_adult=false&original_language=en-US&page=1&query=${search}`;
+  fetch(movieURL, movieAuth)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-            console.log(data.results)
-            showMovies(data.results)
-        
+      console.log(data.results);
+      showMovies(data.results);
     });
 }
+
+fetch("https://api.themoviedb.org/3/configuration", movieAuth)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
 
 function showMovies(movies) {
   main.innerHTML = "";
   movies.forEach((movie) => {
     const { title, overview, poster_path } = movie;
     const movieEl = document.createElement("div");
-    movieEl.innerHTML = `
-    <img src="${imagePath+poster_path}" alt ="${title}/>
-    <div class = "movieInfo">
-    <h3>${title}</h3>
-    <div class="overview">
-    <h3>Overview</h3>
-    ${overview}
-    </div>
-    </div>
-        `;
-        main.appendChild(movieEl)
+    if (poster_path == null) {
+      movieEl.innerHTML = `
+      <h2>${title}</h2>
+      <div class = "movieInfo">
+      <img id="placeholder" src="./placeholder.png" alt ="${title}" /> 
+      <div class="overview">
+      <h3>Overview</h3>
+      ${overview}
+      </div>
+      </div>
+          `;
+    } else if (overview == "") {
+      movieEl.innerHTML = `
+      <h2>${title}</h2>
+      <div class = "movieInfo">
+      <img id="placeholder" src="./placeholder.png" alt ="${title}" /> 
+      <div class="overview">
+      <h3>Overview</h3>
+      <p><i>Overview is not available<i></p>
+      </div>
+      </div>
+          `;
+    } else {
+      movieEl.innerHTML = `
+            <h2>${title}</h2>
+            <div class = "movieInfo">
+            <img src="${imagePath + poster_path}" alt ="${title}" />
+            <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+            </div>
+            </div>
+                `;
+    }
+    main.appendChild(movieEl);
   });
 }
 
-submitBtn.addEventListener("click", function(event) {
+submitBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  if (formInput && formInput !== "") {
-    findMovies(movieURL, movieAuth);
+  const searchInput = formInput.value.trim();
+  if (searchInput && searchInput !== "") {
+    findMovies(searchInput);
     formInput = "";
   } else {
     alert("Please enter a search term"); //change to modal or something
@@ -63,14 +91,9 @@ submitBtn.addEventListener("click", function(event) {
 //   .catch((err) => console.error(err));
 
 fetch(
-  "https://api.themoviedb.org/3/search/movie?query=clash&include_adult=false&original_language=en-US&page=1",
+  "https://api.themoviedb.org/3/search/movie?query=flash&include_adult=false&original_language=en-US&page=1",
   movieAuth
 )
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
-
-// fetch("https://api.themoviedb.org/3/configuration", movieAuth)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
