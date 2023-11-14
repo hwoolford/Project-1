a8303bbbe69ee7d3187351a6c3a056ed60981b47
Adam Smith <asmith5144@hotmail.com>
2023-11-14 12:33:42 -0500

    added loop for viewing book results

--- assets/Test/TestAdamscript.js
+++ assets/Test/TestAdamscript.js
@@ -1,38 +1,47 @@
-$(document).ready(function () {
-    const item, title, author, publisher, booklink, bookImg
-    const outputlist = document.getElementById("list-ouput");
-    const bookUrl = "https://openlibrary.org/subjects/history"
-    const placeHldr = ""
-    const searchData;
 
-    $("#search").click(function () {
-        outputlist.innerHTML = ""
-        searchData = $("#search-box").val();
+// const typeArray = [item, title, author, publisher, booklink, bookImg]
+const outputlist = $("#list-output");
 
-        if (searchData === "" || searchData === null) {
-            displayError();
-        }
-        else {
-            $.ajax({
-                url: bookUrl + searchData,
-                dataType: "json",
-                success: function (res) {
+const placeHldr = ""
+let searchData;
+
+$("#search").on("click", function () {
+    outputlist.innerHTML = ""
+    searchData = $("#search-box").val();
+    const bookUrl = `https://openlibrary.org/subjects/${searchData}`
+    if (searchData) {
+        $.ajax({
+            url: bookUrl,
+            dataType: "json",
+            success: function (res) {
+                console.log(res)
+                if (res.work_count === 0) {
+                    alert("no results.. try again")
+                }
+                else {
+                    for (let index = 0; index < res.works.length; index++) {
+                        const book = res.works[index];
+                        const img = $("<img>");
+                        img.attr("src", "https://covers.openlibrary.org/b/olid/" + book.cover_edition_key + "-M.jpg");
+                        outputlist.append(img);
+                    }
                     console.log(res)
-                    if (response.totalItem === 0) {
-                        alert("no results.. try again")
-                    }
-                    else {
-                        $("title").anitem({ 'margin-top: 10px'});
-                        $(".book-list").css('visibility: visible');
-                        displayResults(res);
-                    }
                 }
-                error: function () {
-                    alert("Something went wrong...");
-                }
-            })
-        }
-        $("#search-box").val("");
-    })
+            },
+            error: function () {
+                alert("Something went wrong...");
+            }
+        });
+    }
+    // clears search box
+    $("#search-box").val("");
+});
 
-})
+// function to display results in index.html @param res
+function displayResults(res) {
+    for (var i = 0; i < res.items.length; i += 2) {
+        item = res.items[i];
+        title1
+    }
+}
+
